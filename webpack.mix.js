bc7cac430302cb447b1af27106da2d2c0d3bd8de
Ndo <fransiscusrmalau@gmail.com>
2020-11-18 21:44:01 +0700

    “wip”

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,4 @@ const mix = require('laravel-mix');
 mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/main.sass', 'public/css')
     .sourceMaps();
