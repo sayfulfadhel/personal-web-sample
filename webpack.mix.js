59bbbaa33439f2cea2af9830067c903c4f1178f4
Ndo <fransiscusrmalau@gmail.com>
2020-11-18 22:30:42 +0700

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,4 @@ mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+mix.version();
